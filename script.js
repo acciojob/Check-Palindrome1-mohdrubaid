9198e2fb6cfaee984dbcf6e6ae5974514a623633
mohdrubaid <125140108+mohdrubaid@users.noreply.github.com>
2023-04-06 00:15:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-
+str=str.toLoereCase().replace(/[^a-z0-9]/g, "");
+	return syr===str.split("").reverse().join("");
 }
 module.exports = palindrome
